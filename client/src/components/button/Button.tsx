08e7c29b6c788cbf2edce7e5721a28d9f430f0f3
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-08-14 01:43:04 +0200

    add: tasks in TaskSection component

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 }
 
 export const Button: (props: ButtonProps) => JSX.Element = (props: ButtonProps): JSX.Element => {
-  const buttonClass: string =clsx(
+  const buttonClass: string = clsx(
     'py-2 px-4 rounded-md bg-highlight-bg outline-none cursor-pointer',
     'font-xl font-semibold text-text-secondary',
     'focus:bg-focus-bg hover:bg-focus-bg'
--- client/src/components/main/TaskSection.tsx
+++ client/src/components/main/TaskSection.tsx
@@ -1,7 +1,8 @@
-import type {JSX} from 'react';
+import {type JSX} from 'react';
 import clsx from 'clsx/lite';
 import {type List} from "../../data/lists.ts";
 import {Button} from "../button/Button.tsx";
+import {type Task} from "../../data/tasks.ts";
 
 type TaskSectionProps = {
   lists: Array<List>
@@ -17,14 +18,18 @@ export const TaskSection: (props: TaskSectionProps) => JSX.Element = (props: Tas
       scrollbar-thumb-gray-400 scrollbar-track-transparent`,
     !props.lists.length && 'justify-center items-center text-center'
   )
-  const tasksSectionClassName: string = clsx(
-    'flex flex-1 bg-secondary-bg rounded-md',
-    !props.selectedListId && 'justify-center items-center text-center'
+  const tasksListClassName: string = clsx(
+    'flex flex-col flex-1 bg-secondary-bg rounded-md gap-2 pr-1 overflow-y-auto ',
+    'scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent',
   )
-
+  const rightSectionClassName: string = clsx(
+    'flex flex-col flex-1 min-w-[507px] bg-secondary-bg rounded-md',
+    !props.selectedListId && 'justify-center items-center text-center',
+    props.selectedListId && 'justify-between gap-2 p-4'
+  )
   return (
     <>
-      <section className="flex flex-col flex-1 justify-between gap-2 bg-secondary-bg rounded-md p-4">
+      <section id="sec1" className="flex flex-col flex-1 min-w-[507px] justify-between gap-2 bg-secondary-bg rounded-md p-4">
         <h2 className="pb-2 border-b border-text-secondary text-2xl font-semibold text-text-primary">
           My lists
         </h2>
@@ -55,8 +60,38 @@ export const TaskSection: (props: TaskSectionProps) => JSX.Element = (props: Tas
           />
         </div>
       </section>
-      <section className={tasksSectionClassName}>
-        {props.selectedListId ? props.selectedListId : (
+      <section className={rightSectionClassName}>
+        {props.selectedListId ? (
+          <>
+            <h2 className="pb-2 border-b border-text-secondary text-2xl font-semibold text-text-primary">
+              {props.lists
+                .find((list: List): boolean =>
+                  list.id === props.selectedListId)!.title
+              }
+            </h2>
+            <section className={tasksListClassName}>
+              {props.lists
+                .find((list: List): boolean => list.id === props.selectedListId)!.tasks
+                .map((task: Task): JSX.Element => (
+                  <div
+                    key={task.id}
+                    className="flex flex-1 p-2 bg-gray-300 rounded-md hover:border hover:border-gray-400"
+                  >
+                    <section className="flex flex-col">
+                      <h3 className="text-text-primary text-base font-semibold">{task.title}</h3>
+                      <p className="text-text-secondary text-sm font-medium">{task.description}</p>
+                    </section>
+                  </div>
+                ))}
+            </section>
+            <div className="flex justify-end">
+              <Button
+                text={"Add task"}
+                clickHandler={() => console.log('add task')}
+              />
+            </div>
+          </>
+        ) : (
           <span className="inline-block w-3/4 text-4xl text-gray-400">
             Choose a task list to see its tasks...
           </span>
