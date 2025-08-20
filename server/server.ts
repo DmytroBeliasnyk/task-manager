d1cd09ad647f28d5c4fdf8100f2ab4273eed4fc8
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-08-20 02:15:58 +0200

    impl: add list endpoint

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import {apiRouter} from "./routes/apiRouter";
 const PORT: number = 8000
 const app: Application = express()
 
+app.use(express.json())
 app.use('/api', apiRouter)
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
--- server/services/list.ts
+++ server/services/list.ts
@@ -1,5 +1,13 @@
 import {List} from "../../shared/types/list";
-import {getListsFromDB} from "../repo/list";
+import {getListsFromDB, saveListInDB} from "../repo/list";
+import {nanoid} from "nanoid";
+
+export async function saveList(title: string, description: string): Promise<string> {
+  const id: string = nanoid()
+  await saveListInDB(id, title, description)
+
+  return id
+}
 
 export async function getLists(): Promise<List[]> {
   return await getListsFromDB()
